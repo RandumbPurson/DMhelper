23aaab91b4ff2f6119983b6ea2e232709d735572
Emma Fountain <randumbemma@gmail.com>
2024-01-21 09:37:16 -0500

    Add statblock speeds

--- src/components/statblock/StatblockTopInfo.tsx
+++ src/components/statblock/StatblockTopInfo.tsx
@@ -6,17 +6,25 @@ interface Props {
     state: stateType;
 }
 
+function speedsString(speeds) {
+    return Object.entries(speeds).map(spd => `${spd[0]} ${spd[1]} ft`).join(",")
+}
+
 export default function StatblockTopInfo({traits, state}: Props) {
     console.log(traits, state)
     return (
         <div className="top-stats">
             <div className="property-line first">
-                <h4>Armor Class </h4>
-                <p>{traits["AC"]}</p>
+                <h4>Armor Class</h4>
+                <p> {traits["AC"]}</p>
             </div>
             <div className="property-line">
                 <h4>Hit Points </h4>
-                <p>{`${state["HP"]} / ${state["maxHP"]}`}</p>
+                <p>{`${state["HP"]} / ${state["maxHP"]} ${traits["HPDice"] ? traits["HPDice"] : ""}`}</p>
+            </div>
+            <div className="property-line">
+                <h4>Speed </h4>
+                <p>{speedsString(traits["speed"])}</p>
             </div>
         </div>
     );
